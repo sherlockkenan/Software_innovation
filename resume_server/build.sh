8cafceb269d9bfb85cc23e3f2dde32a28adeb17d
luxu <1603155230@qq.com>
2019-06-23 21:00:56 +0800

    add some jsonresume templates

--- resume_server/build.sh
+++ resume_server/build.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-hackmyresume BUILD ./resume-jrs.json TO ./index.html -t ./node_modules/jsonresume-theme-onepage
-hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-onepage
+#hackmyresume BUILD ./resume-jrs.json TO ./index.html -t ./node_modules/jsonresume-theme-onepage
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-onepage
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-class
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-classy
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-flat
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-kendall
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-modern
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-onepage-year
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-paper
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-short
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-slick
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-spartan
+#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-stackoverflow
+hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-elegant
 
-open ./index.html
+#open ./index.html
 open ./cn/index.html
---
+++ resume_server/node_modules/jsonresume-theme-slick/README.md
@@ -0,0 +1,32 @@
+# Slick theme
+
+This is a theme for [JSON Resume](http://jsonresume.org/).
+
+## See a preview
+Take a look at my resume [JSON Resume](http://registry.jsonresume.org/dfmcphee).
+
+## Supported Social Profiles
+* github
+* flickr
+* vimeo
+* twitter
+* facebook
+* gplus
+* google-circles
+* pinterest
+* tumblr
+* linkedin
+* dribble
+* rdio
+* spotify
+* soundcloud
+* instagram
+* skype
+
+## License
+
+Available under [the MIT license](http://mths.be/mit).
+
+## Attributions
+
+Entypo pictograms by Daniel Bruce — [entypo.com](www.entypo.com)
